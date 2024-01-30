8b63aca4200a089ffaeb929f4d9e620c5906ae19
Younisprogramming <younistaher12341@gmail.com>
2024-01-30 11:55:39 -0800

    task0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_node - we missed you betty
+ *@parent: pointer to the parent node of the node to create
+ *@value: the value to put in the new node
+ *
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	if (!parent)
+		return NULL;
+	parent->n = value;
+	parent->left = NULL;
+	parent->right = NULL;
+
+	return parent;
+}
