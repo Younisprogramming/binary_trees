1268f6a2a0dbfd9834aedc177ea627aacc815c20
Younisprogramming <younistaher12341@gmail.com>
2024-02-01 12:10:42 -0800

    task111

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,20 +15,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-	result->left = NULL;
+	result->parent = parent;
+	result->left = parent->left;
 	result->right = NULL;
 	result->n = value;
 
 	if (parent->left != NULL)
 	{
 		parent->left->parent = result;
-		result->left = parent->left;
-		parent->left = result;
 	}
-	else
-	{
-		parent->left = result;
-	}
-	result->parent = parent;
+	parent->left = result;
 	return (result);
 }
