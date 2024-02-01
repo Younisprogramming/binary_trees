431194a7d2b4d484b649fedf9596cdd71e0f134b
Younisprogramming <younistaher12341@gmail.com>
2024-02-01 12:46:31 -0800

    task5

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_is_root - a function
+ *@node: a pointer
+ *Return: pointer
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->parent == NULL)
+		return (1);
+	return (0);
+}
