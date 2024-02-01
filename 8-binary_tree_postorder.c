2651454d27e5b9402c31a8d1b944d2551c0109d0
Younisprogramming <younistaher12341@gmail.com>
2024-02-01 14:34:22 -0800

    task7+8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- *  *binary_tree_postorder - a function
- *   *@tree: a pointer
- *    *@func: value
- *     */
+ *binary_tree_postorder - a function
+ *@tree: a pointer
+ *@func: value
+ */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
