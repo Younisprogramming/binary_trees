578d1aaa23d116f305fbcce86aef8b15216fe31c
Younisprogramming <younistaher12341@gmail.com>
2024-02-01 11:51:34 -0800

    task1111

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *result = malloc(sizeof(binary_tree_t));
 
-	if (!parent)
+	if (parent == NULL)
 		return (NULL);
 
 	if (result == NULL)
