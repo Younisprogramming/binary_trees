db7f169f76b2edf982792044cd00f4a6a6d84597
Younisprogramming <younistaher12341@gmail.com>
2024-02-01 18:07:02 -0800

    a7erannnnnnnnnnnnnnnnnnnnnnn

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_uncle - find the uncle 
+ *@node: A pointer 
+ *Return: wdwadawd
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	return (NULL);
+}
