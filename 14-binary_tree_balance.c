21e21855ec2478ef1639519027defb155945fa04
Younisprogramming <younistaher12341@gmail.com>
2024-02-01 17:51:04 -0800

    task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,22 +1,42 @@
 #include "binary_trees.h"
 /**
- *binary_tree_balance - Measures the balance factor of a binary tree
- *
- *@tree: A pointer
- *
- *Return: dwadwdw
+ *binary_tree_node_height - cawdwad
+ *@node: sdwdwd
+ *@h: dawdwd
+ *Return: Heightdwad
+ */
+
+int binary_tree_node_height(const binary_tree_t *node, int h)
+{
+	int lefth, righth;
+
+	if (node == NULL)
+		return (h);
+
+	lefth = binary_tree_node_height(node->left, h + 1);
+	righth = binary_tree_node_height(node->right, h + 1);
+
+	return (lefth > righth ? lefth : righth);
+}
+/**
+ *binary_tree_balance - Measures the balance factor
+ *@tree: A podawdwadwa
+ *Return: 0 if tree is NULL.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = 0, right_height = 0;
+	int balance_factor = 0, lefth = 0, righth = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		left_height = 1 + binary_tree_balance(tree->left);
-	if (tree->right != NULL)
-		right_height = 1 + binary_tree_balance(tree->right);
+	if (tree->left)
+		lefth = binary_tree_node_height(tree->left, 0);
 
-	return (left_height - right_height);
+	if (tree->right)
+		righth = binary_tree_node_height(tree->right, 0);
+
+	balance_factor = lefth - righth;
+
+	return (balance_factor);
 }
