bbdcb110a40e2c715a4ce3fb05f68afafc12f756
Younisprogramming <younistaher12341@gmail.com>
2024-02-01 18:03:38 -0800

    task16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,24 @@
 #include "binary_trees.h"
 /**
+ *binary_tree_node_height - cawdwad
+ *@node: sdwdwd
+ *@h: dawdwd
+ *Return: Heightdwad
+ */
+int binary_tree_node_height(const binary_tree_t *node, int h)
+{
+	int lefth, righth;
+
+	if (node == NULL)
+		return (h);
+
+	lefth = binary_tree_node_height(node->left, h + 1);
+	righth = binary_tree_node_height(node->right, h + 1);
+
+	return (lefth > righth ? lefth : righth);
+
+}
+/**
  *binary_tree_is_perfect - Cwdawdwdw
  *@tree: A pointadwda
  *Return: 1 if the tree is perfect, 0 otherwise.
